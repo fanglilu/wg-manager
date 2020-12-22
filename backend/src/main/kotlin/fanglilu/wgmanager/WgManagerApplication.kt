965b677575e169e591c698e91283a0862b7b79cb
Henry Pham <h.pham@pripares.com>
2020-12-22 03:01:10 +0100

    #hp22Dez2020 add initial backend

---
+++ backend/src/main/kotlin/fanglilu/wgmanager/WgManagerApplication.kt
@@ -0,0 +1,11 @@
+package fanglilu.wgmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WgManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<WgManagerApplication>(*args)
+}
---
+++ backend/src/test/kotlin/fanglilu/wgmanager/WgManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package fanglilu.wgmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WgManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
